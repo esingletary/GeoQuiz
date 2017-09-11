3bbdef3ae3b8d3c54c0f4b0fa92505257c42542f
Emmanuel Singletary <me@manny.co>
2017-09-10 22:47:49 -0400

    The toast shows at the top now instead of the bottom.

--- app/src/main/java/co/manny/bnr/geoquiz/QuizActivity.java
+++ app/src/main/java/co/manny/bnr/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package co.manny.bnr.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -22,7 +23,10 @@ public class QuizActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View v) {
-                Toast.makeText(QuizActivity.this, R.string.correct_toast,Toast.LENGTH_SHORT).show();
+                Toast correctToast = Toast.makeText(QuizActivity.this, R.string.correct_toast,Toast.LENGTH_SHORT);
+
+                correctToast.setGravity(Gravity.TOP, 0, 0);
+                correctToast.show();
             }
         });
 
@@ -30,7 +34,10 @@ public class QuizActivity extends AppCompatActivity {
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View v) {
-                Toast.makeText(QuizActivity.this, R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
+                Toast incorrectToast = Toast.makeText(QuizActivity.this, R.string.incorrect_toast,Toast.LENGTH_SHORT);
+
+                incorrectToast.setGravity(Gravity.TOP, 0, 0);
+                incorrectToast.show();
             }
         });
 
